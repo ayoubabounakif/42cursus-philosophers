1565f4ceae921e67aecfa74ea29a95550e6ae198
abiari <abiari@student.1337.ma>
2021-07-05 10:32:35 +0100

    added custom usleep

--- srcs/threadsUtils/getCurrentTime.c
+++ srcs/threadsUtils/getCurrentTime.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   getCurrentTime.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabounak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: abiari <abiari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:55:51 by aabounak          #+#    #+#             */
-/*   Updated: 2021/06/30 16:56:12 by aabounak         ###   ########.fr       */
+/*   Updated: 2021/07/05 10:32:19 by abiari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,25 @@ uint64_t	getCurrentTime(void)
 	gettimeofday(&tv, NULL);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
+
+// void	ft_usleep(int timeToSleep)
+// {
+// 	uint64_t	start;
+// 	uint64_t	timeDiff;
+
+// 	start = getCurrentTime();
+// 	while (1)
+// 	{
+// 		timeDiff = timeToSleep - (getCurrentTime() - start);
+// 		if (timeDiff < 0)
+// 			break ;
+// 		if (timeDiff < 50)
+// 		{
+// 			usleep(timeDiff);
+// 			break ;
+// 		}
+// 		if (timeDiff > (uint64_t)timeToSleep)
+// 			break ;
+// 		usleep(50);
+// 	}
+// }
